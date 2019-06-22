60862a682fd0e2bbf4f67f97b86a52ac2dd784d6
Kayley <kayley.luftig@gmail.com>
2019-06-22 03:18:49 +0000

    Create custom method to display helper function for store name using props

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { getFunName } from '../helpers.js';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. stop form from submiting
+    event.preventDefault()
+    // 2. get the text from that input
+    console.log(this.myInput)
+    //3. change the page to /store/whatever-they-entered
+  }
+
   render() {
     return  (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a Store</h2>
-        <input  type="text" required placeholder="Store Name" />
+        <input ref={this.myInput} type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store → </button>
       </form>
     )
