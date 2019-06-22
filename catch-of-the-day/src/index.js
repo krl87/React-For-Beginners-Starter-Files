631affbdff1eafbfb100d5ddac2d2421b249181b
Kayley <kayley.luftig@gmail.com>
2019-06-22 00:07:26 +0000

    Creating pages/props

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './components/Router.js';
+import './css/style.css';
+
+render(<Router />, document.getElementById('main'))
